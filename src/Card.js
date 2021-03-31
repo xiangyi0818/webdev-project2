6fe2255a296bcf1dbc95bfb878bfeff436e81c6e
xiangyi0818 <34527543+xiangyi0818@users.noreply.github.com>
2021-03-30 20:42:14 -0700

    modify file

---
+++ src/Card.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Reducer from './Reducer';
+import "./Card.css";
+
+export default class Card extends React.Component {
+    constructor(props){
+      super(props);
+    }
+
+    render(){
+        // console.log(this.props.type)
+        // let testStr = mapCardToName(this.props.type)
+        // console.log(testStr)
+        // console.log("./images/".concat(mapCardToName(this.props.type)))
+        return(
+            <div className= {this.props.active? "Card active":"Card"}
+            style={{border:"1.5px solid grey"}} 
+            onClick={() => this.props.addCard(this.props.type)} active={true}>
+                {mapCardToName(this.props.type)}
+            {/* <img src={require("./images/".concat(mapCardToName(this.props.type)))} /> */}
+            {/* {this.props.type.map((value, index) => <p>{value}</p>)} */}
+            </div>
+        )
+    }
+    }
+
+    let mapCardToName = (cardType) => {
+        let dict1={0:"Diamond",1:"Round",2:"Squiggle"} 
+        let dict2={0:"Red",1:"Purple",2:"Green"}
+        let dict3={0:"Solid",1:"Empty",2:"GrayedOut"}
+        let dict4={0:"One",1:"Two",2:"Three"}
+        // console.log(cardType)
+        // console.log("./images/".concat(dict1[cardType[0]].concat("_",dict2[cardType[1]],"_",dict3[cardType[2]],"_",dict4[cardType[3]],".png")))
+        return <img src={window.location.origin +"/images/".concat(dict1[cardType[0]].concat("_",dict2[cardType[1]],"_",dict3[cardType[2]],"_",dict4[cardType[3]],".png"))} width="100%" height="100%"/>
+            // (dict1[cardType[0]].concat("_",dict2[cardType[1]],"_",dict3[cardType[2]],"_",dict4[cardType[3]],".png"))      
+      }
+
---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {connect} from "react-redux";
+import { useSelector } from 'react-redux';
+import './App.css';
+import Card from "./Card";
+import {isInToRemove} from "./Reducer";
+import {Link} from 'react-router-dom';
+
+
+export default class Home extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    console.log("Home")
+    return(
+    <div className="home">
+    <div>
+      <button> <Link to={"/home"}>Home</Link></button>
+      <button> <Link to={""}>Difficulty</Link></button>
+      <button> <Link to={"/rule"}>Rule</Link></button>
+    </div>
+    <div>
+      <p>Press Difficulty button for level options.</p>
+    </div>
+    <div>
+      <p>Press Rule button for game rules.</p>
+    </div>
+    <div>
+      <p>Let's get started!</p>
+    </div> 
+  </div>  
+    )
+  }
+}
